4135979edcdac0728f86b74f127b0269216f3136
rahul1269-kaushal <59966780+rahul1269-kaushal@users.noreply.github.com>
2024-03-16 16:29:03 +0530

    Update views.py

--- vercel_assignment/views.py
+++ vercel_assignment/views.py
@@ -8,3 +8,12 @@ def q1(request):
 
 def q2(request):
     return render(request, 'questions/2.html')
+
+def q3(request):
+    return render(request, 'questions/3.html')
+
+def q4(request):
+    return render(request, 'questions/4.html')
+
+def q5(request):
+    return render(request, 'questions/5.html')
